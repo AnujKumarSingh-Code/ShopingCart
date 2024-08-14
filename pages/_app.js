7e207b229840c7e66299642b97b53d8e45f10294
Anuj Kumar <anujkumarcoder@gmail.com>
2024-08-14 16:02:57 +0530

    More

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,7 @@ import { CartProvider } from '../contexts/CartContext';
 function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-      <script src="https://ipinfo.io/widget"></script>
-
+      
       <Component {...pageProps} />
     </CartProvider>
   );
