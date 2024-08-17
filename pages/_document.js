8782785562be7eea5dec6dc8410d9fd9968eb139
Anuj Kumar <133045218+AnujKumarSingh-Code@users.noreply.github.com>
2024-08-17 15:42:47 +0530

    Update ing_document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-// pages/_document.js
+
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -9,11 +9,19 @@ class MyDocument extends Document {
           {/* Google Tag Manager */}
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M8P49DBX');`,
+              __html: `
+                (function(w,d,s,l,i){
+                  w[l]=w[l]||[];
+                  w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});
+                  var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),
+                      dl=l!='dataLayer'?'&l='+l:'';
+                  j.async=true;
+                  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                  f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-M8P49DBX');
+              `,
             }}
           />
           {/* End Google Tag Manager */}
@@ -21,8 +29,10 @@ class MyDocument extends Document {
         <body>
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8P49DBX"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+              __html: `
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8P49DBX"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `,
             }}
           />
           <Main />
