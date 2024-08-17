9168c1de591d4cdb644db83dd284e01ca2afcf0d
Anuj Kumar <133045218+AnujKumarSingh-Code@users.noreply.github.com>
2024-08-17 13:21:04 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MZKB5SXK');`,
+              })(window,document,'script','dataLayer','GTM-M8P49DBX');`,
             }}
           />
           {/* End Google Tag Manager */}
@@ -21,7 +21,7 @@ class MyDocument extends Document {
         <body>
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZKB5SXK"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8P49DBX"
                 height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
