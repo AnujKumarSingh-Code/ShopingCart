6cc7a0f13e6237b53180503329425da26adfd197
Anuj Kumar <133045218+AnujKumarSingh-Code@users.noreply.github.com>
2024-08-17 13:14:53 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,33 +6,42 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Global Tag Manager Script */}
+      {/* Google Tag Manager */}
       <Script
-        id="gtm-script"
+        id="google-tag-manager"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MZKB5SXK');`,
-        }}
-      />
-      {/* CartProvider context */}
+      >
+        {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M8P49DBX');
+        `}
+      </Script>
+      {/* End Google Tag Manager */}
+
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-M8P49DBX"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
+
       <CartProvider>
         <Component {...pageProps} />
       </CartProvider>
     </>
-
-import '../styles/globals.css';
-import { CartProvider } from '../contexts/CartContext';
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <CartProvider>
-      <Component {...pageProps} />
-    </CartProvider>
-
   );
 }
 
